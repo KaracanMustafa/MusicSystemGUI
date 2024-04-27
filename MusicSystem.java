1cac33f7dba46109dba53ef0b05295f1ea487aad
KaracanMustafa <163058354+KaracanMustafa@users.noreply.github.com>
2024-04-28 01:41:51 +0300

    Add files via upload

---
+++ MusicSystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package musicsystem;
+
+/**
+ *
+ * @author karac
+ */
+public class MusicSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       entryPage ep1=new entryPage();
+       ep1.setVisible(true);
+    }
+    
+}
